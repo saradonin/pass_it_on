55337f37dbf19a50ef346ca91a87b887d5112c05
saradonin <m.jemieljanczuk@gmail.com>
2023-10-15 16:47:20 +0200

    Institution model created and migrated

--- pass_it_on_app/models.py
+++ pass_it_on_app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Represents a category.
+    """
+    name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        """
+        Return a string representation of the category.
+        """
+        return self.name
+
+
+class Institution(models.Model):
+    """
+    Represents an institution.
+    """
+    TYPE_CHOICES = {
+        (1, "fundacja"),
+        (2, "organizacja pozarządowa"),
+        (3, "zbiórka lokalna")
+    }
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True)
+    type = models.CharField(max_length=64, choices=TYPE_CHOICES, default=1)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        """
+        Return a string representation of the institution.
+        """
+        return self.name
+
