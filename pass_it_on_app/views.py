73cd7abff77f8a836cc7460a3f4a8da7fa5ba0cf
saradonin <m.jemieljanczuk@gmail.com>
2023-10-15 14:01:56 +0200

    navbar moved to external file

--- pass_it_on_app/views.py
+++ pass_it_on_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+class IndexView(View):
+    """
+    View for rendering the index page.
+    """
+
+    def get(self, request):
+        return render(request, 'index.html')
